5fe9cddf82f71f5558ff38b10500e001b82e91c0
alghailaniyoussef <63921184+alghailaniyoussef@users.noreply.github.com>
2025-03-02 02:25:36 +0100

    Añadir vista de selección de película

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,9 @@
     <meta charset="UTF-8">
     <title>SWAPI Project</title>
 
-    {{-- Si usas Vite en Laravel 9+ o 10+ con Tailwind --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    {{-- Si no usas Vite, podrías incluir Tailwind así:
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    --}}
+
 </head>
 <body class="bg-gray-100 text-gray-800">
 
--- resources/views/swapi/index.blade.php
+++ resources/views/swapi/index.blade.php
@@ -34,6 +34,7 @@
                     </select>
                 </div>
 
+
                 <button
                     type="submit"
                     class="bg-blue-600 text-white px-4 py-2 rounded
